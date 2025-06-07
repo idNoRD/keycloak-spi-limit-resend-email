d605ddb3db0896b26562dfaed9189c2c63efc132
idNoRD <idNoRD@users.noreply.github.com>
2025-06-06 21:37:00 -0700

    init mvn

---
+++ src/main/java/idnord/keycloak/App.java
@@ -0,0 +1,13 @@
+package idnord.keycloak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
